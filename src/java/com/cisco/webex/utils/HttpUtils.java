6e9ee806d9ed989dc8fd77190617592badba4ddb
Jackycheug <kinchen@cisco.com>
2014-02-18 17:57:58 +0800

    project committed

---
+++ src/java/com/cisco/webex/utils/HttpUtils.java
@@ -0,0 +1,12 @@
+package com.cisco.webex.utils;
+/**   
+ * @Title: HttpUtils.java
+ * @Package java.com.cisco.webex.utils
+ * @Description: TODO
+ * @author King chen
+ * @date Feb 18, 2014 5:50:34 PM    
+ * @version V1.0   
+ */
+public class HttpUtils {
+
+}
