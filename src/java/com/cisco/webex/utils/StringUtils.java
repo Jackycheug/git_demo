d4ffe0eef970badd41b1afbe1d7082031ee1f0a4
Jackycheug <kinchen@cisco.com>
2014-02-19 15:22:23 +0800

    add a new class

---
+++ src/java/com/cisco/webex/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package com.cisco.webex.utils;
+/**   
+ * @Title: StringUtils.java
+ * @Package com.cisco.webex.utils
+ * @Description: TODO
+ * @author King chen
+ * @date Feb 19, 2014 3:14:44 PM    
+ * @version V1.0   
+ */
+public class StringUtils {
+
+}
